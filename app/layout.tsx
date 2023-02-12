836bc1829162109fccca30fcd7b9bbc86cfd3175
ossany.insua <ossany.insua1982@gmail.com>
2023-02-12 13:26:14 -0300

    Add fonts to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Header from '../components/header/header'
+import { font } from './font';
 
 export default function RootLayout({
   children,
@@ -13,7 +14,7 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={font.className}>
         <Header />
         {children}
       </body>
--- components/comment/index.tsx
+++ components/comment/index.tsx
@@ -1,4 +1,5 @@
 import { fetchCommentsPost } from '@/services/fetch'
+import Image from 'next/image';
 import React from 'react'
 import classes from './commentPost.module.css'
 
@@ -19,6 +20,7 @@ export default async function CommentsPost({ id }: {id: string}) {
             <li key={id}>
               <article className={classes.article} >
                 <h4 style={{marginBottom: "1rem"}}>{name}</h4>
+                <Image width={40} height={40} src={`https://www.gravatar.com/avatar/${name}?d=retro&f=y&s=128`} alt="avatar" />
                 <p style={{marginBottom: "1rem", fontStyle: "italic"}}>{email}</p>
                 <p style={{marginBottom: "1rem"}}>{body}</p>
               </article>
